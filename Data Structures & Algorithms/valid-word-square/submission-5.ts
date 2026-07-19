e4039677cbf0fa318d9c9b878aca1b1b7b9dcb15
Vikash Singh <36200132+vikash5529@users.noreply.github.com>
2026-07-19 11:21:23 +0530

    Add: valid-word-square - submission-5

---
+++ Data Structures & Algorithms/valid-word-square/submission-5.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @return {boolean}
+     */
+    validWordSquare(words: string[]): boolean {
+        for(let i=0;i<words.length; i++){
+            for(let j=0;j<words[i].length;j++) {
+               if(words[i]?.charAt(j) !== words[j]?.charAt(i)) {
+                return false;
+               }
+            }
+        }
+      return true  
+    }
+}
