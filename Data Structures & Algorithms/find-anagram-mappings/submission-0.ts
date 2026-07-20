3a73223945d6d0c0ac8d9d9cc3bcb723943428a3
Vikash Singh <36200132+vikash5529@users.noreply.github.com>
2026-07-20 09:15:50 +0530

    Add: find-anagram-mappings - submission-0

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-0.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    anagramMappings(nums1: number[], nums2: number[]): number[] {
+      const map  = new Map();
+      const res =[];
+      for(let i=0;i<nums2.length;i++){
+        map.set(nums2[i],i);
+      }
+     for(let i=0;i<nums1.length;i++){
+        res.push(map.get(nums1[i]))
+     }
+    return res;
+    }
+}
+
